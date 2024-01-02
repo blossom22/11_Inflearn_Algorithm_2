a15161a8281196660541fb47820c5a7e8ea7c7d4
blossom22 <ssunyaang@naver.com>
2024-01-02 21:15:47 +0900

    modified

--- 3_Implementation_3.py
+++ 3_Implementation_3.py
@@ -12,3 +12,16 @@ for i in range(n):
                 # set자료형에서 add함수는 list에서 append같은 것이다
                 answer.add(nlist[i]+nlist[j]+nlist[m])
 print(sorted(answer)[-k])
+
+
+
+
+
+# 참고사항
+# 최소값 구하는 알고리즘: 임의의 큰 수를 설정하고 선형탐색하면서 arrMin을 업데이트한다
+arr = [5,3,7,9,2,5,2,6]
+arrMin = float('inf')   # 옆의 이거는 파이썬에서 들어올 수 있는 가장 큰 값이 들어온다.
+for i in range(len(arr)):
+    if arrMin>arr[i]:
+        arrMin = arr[i]
+print(arrMin)
