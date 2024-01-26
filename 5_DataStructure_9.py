4eb97fb5a45524487dd32fc68f4cb4e916d953aa
blossom22 <ssunyaang@naver.com>
2024-01-26 17:41:23 +0900

    Solution added

--- 5_DataStructure_9.py
+++ 5_DataStructure_9.py
@@ -29,3 +29,29 @@ for x in a:
         break
 else:
     print("YES")
+
+
+## 아래는 딕셔너리를 쓰지 않고 리스트로 풀어본 풀이이다 (아스키넘버 활용)
+a = input()
+b = input()
+str1 = [0]*52   # 알파벳이 26개(대소문자 구분하면 52개)
+str2 = [0]*52
+for x in a:
+    if x.isupper():
+        # 대문자 알파벳은 65~90번 아스키넘버이므로, 이를 0~25번 인덱스로 만들려면 65를 빼야함
+        str1[ord(x)-65] += 1
+    else:
+        # 소문자 알파벳은 97~122번이므로, 71을 빼야 인덱스번호가 26~51로 만들어진다
+        str1[ord(x)-71] += 1
+for x in b:
+    if x.isupper():
+        str2[ord(x)-65] += 1
+    else:
+        str2[ord(x)-71] += 1
+
+for i in range(52):
+    if str1[i] != str2[i]:
+        print('NO')
+        break
+else:
+    print("YES")
